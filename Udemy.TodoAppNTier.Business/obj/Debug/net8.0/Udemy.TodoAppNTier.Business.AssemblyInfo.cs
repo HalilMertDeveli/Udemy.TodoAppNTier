aadac41ce8fd93091f80ccafb1bf4ac3f9ad0b96
HMDSS <halilmertdeveliii@gmail.com>
2024-10-17 00:55:30 +0300

    no found page has been added

--- Udemy.TodoAppNTier.Business/obj/Debug/net8.0/Udemy.TodoAppNTier.Business.AssemblyInfo.cs
+++ Udemy.TodoAppNTier.Business/obj/Debug/net8.0/Udemy.TodoAppNTier.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Udemy.TodoAppNTier.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16be57cb4b7d0f602577664e673bd787f8b481a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b2d99c37ce1c41f2532d06579ef36a888f6217a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Udemy.TodoAppNTier.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Udemy.TodoAppNTier.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
